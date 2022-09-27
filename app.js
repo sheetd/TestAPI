52c08143c89abcaf680052d4e01b9432d5632b89
sheetd <neil@sheetd.com>
2022-09-26 22:46:59 -0400

    Docker setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    res.json(["Tony","Lisa","Michael","Ginger","Neil"]);
     });
 
 app.listen(3000, () => {
