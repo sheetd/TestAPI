a450c34409d611e173b6e23169dccc55da16b489
sheetd <neil@sheetd.com>
2024-02-27 18:17:28 -0500

    formatting

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 
 app.get("/purple", (req, res, next) => {
-    res.json(["Wendy","Lisa","Prince","Dez"]);
+  res.json(["Wendy", "Lisa", "Prince", "Dez"]);
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
